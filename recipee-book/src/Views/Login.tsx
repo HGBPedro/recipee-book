266ad32d4c97b1808667025b2392e31c9580e6f1
Pedro H <pedro.bonel@gmail.com>
2022-12-13 00:17:43 -0300

    Adding some dependencies and setting up route

---
+++ recipee-book/src/Views/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <form action="">
+      <label htmlFor='login'>E-Mail</label>
+      <input name='login' type="text" />
+    </form>
+  )
+}
+
+export default Login
