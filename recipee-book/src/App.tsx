a1fb0898dfa5242fac8cb7f10f862936fad2f20c
Pedro H <pedro.bonel@gmail.com>
2023-01-28 18:39:15 -0300

    Will learn to handle everything ejected

--- recipee-book/src/App.tsx
+++ recipee-book/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import './App.less';
 import router from './Routes';
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router}/>
+      <p> Testing </p>
     </div>
   );
 }
--- recipee-book/src/Views/Login.tsx
+++ recipee-book/src/Views/Login.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 function Login() {
+
+  const onSubmit = (values: React.SyntheticEvent) => {
+    console.log(values)
+  }
   return (
-    <form action="">
+    <div className="login-screen">
+    <form onSubmit={onSubmit}>
       <label htmlFor='login'>E-Mail</label>
       <input name='login' type="text" />
+
+      <label htmlFor='login'>Senha</label>
+      <input name='password' type="password" />
+
+      <button type='submit'>Enviar</button>
     </form>
+    </div>
   )
 }
 
