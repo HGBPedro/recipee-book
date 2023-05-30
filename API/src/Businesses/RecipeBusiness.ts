3df6328cb8b33774429079029fdcdcb60b82b388
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-05-29 21:38:28 -0300

    Using aggregate on searches

--- API/src/Businesses/RecipeBusiness.ts
+++ API/src/Businesses/RecipeBusiness.ts
@@ -1,18 +1,25 @@
-import { FilterQuery, ProjectionType } from 'mongoose'
+import { FilterQuery, PipelineStage, ProjectionType } from 'mongoose'
 import { IRecipe } from '../Interfaces/IRecipe'
 import RecipeModel from '../Models/recipeModel'
 
+const checkIfObejctIsEmpty = (obj: Object) => Object.keys(obj)?.length > 0 
+
 async function fetchRecipes(filter: FilterQuery<IRecipe>) {
   try {
-    const { nome, _id, tempoPreparo, onlyName } = filter
+    const { nome, _id, tempoPreparo, onlyName, limit = 10 } = filter
     const queryFilter: FilterQuery<IRecipe> = {}
     const projection: ProjectionType<IRecipe> = {}
     if (_id) queryFilter._id = _id
     if (nome) queryFilter.nome = new RegExp(nome, 'i')
     if (tempoPreparo) queryFilter.tempoPreparo = tempoPreparo
     if (onlyName) projection.nome = 1
-    
-    return RecipeModel.find(queryFilter, projection)
+
+    const aggregatePipeline: PipelineStage[] = []
+    if (checkIfObejctIsEmpty(queryFilter)) aggregatePipeline.push({ $match: queryFilter })
+    if (checkIfObejctIsEmpty(projection)) aggregatePipeline.push({ $project: projection })
+    aggregatePipeline.push({ $limit: Number(limit) })
+
+    return RecipeModel.aggregate(aggregatePipeline).exec()
   } catch (err) {
     return err
   }
@@ -22,7 +29,6 @@ async function createRecipe(recipe: IRecipe) {
   try {
     await RecipeModel.create(recipe, (error: any, recipe: IRecipe) => {
       if (error) throw error
-
       return recipe
     })
   } catch (err) {
