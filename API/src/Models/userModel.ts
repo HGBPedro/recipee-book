547ebb344b39ab5515652381d47fac393463564d
Pedro H G Bonel <pedro.bonel@gmail.com>
2023-06-06 21:26:18 -0300

    [REF] Remove unused files

--- API/src/Models/userModel.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-import IUser from '../Interfaces/IUser'
-import usersSchema from '../Schemas/usersSchema'
-
-const UserModel = mongoose.model<IUser>('User', usersSchema)
-
-export default UserModel
